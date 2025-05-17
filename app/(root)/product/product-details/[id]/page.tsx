700c0ffbc0202708e77cec85a96355b5c5bd968f
Navas . M <navasmhd79@gmail.com>
2025-05-17 18:43:09 +0530

    deployment error solved

--- app/(root)/product/product-details/[id]/page.tsx
+++ app/(root)/product/product-details/[id]/page.tsx
@@ -2,32 +2,35 @@ import { getProductByCategory, getSingleProduct } from "@/Request/requests";
 import ProductDetailsClient from "../../ProductDetailsClient";
 import { Product } from "@/typing";
 
-const ProductDetails = async ({ params }: { params: { id: string } }) => {
-  const id = params.id;
-  let singleProduct: Product | null = null;
-  let relatedProduct: Product[] = [];
+interface ProductDetailsProps {
+    params: {
+        id: string;
+    };
+    searchParams?: Record<string, string | string[]>;
+}
+const ProductDetails = async ({ params }: ProductDetailsProps) => {
+    const id = params.id;
+    let singleProduct: Product | null = null;
+    let relatedProduct: Product[] = [];
 
-  try {
-    singleProduct = await getSingleProduct(id);
-    if (singleProduct) {
-      relatedProduct = await getProductByCategory(singleProduct.category);
+    try {
+        singleProduct = await getSingleProduct(id);
+        if (singleProduct) {
+            relatedProduct = await getProductByCategory(singleProduct.category);
+        }
+    } catch (error) {
+        console.error("Error fetching product data:", error);
     }
-  } catch (error) {
-    console.error("Error fetching product data:", error);
-  }
 
-  if (!singleProduct) {
-    return <div>Product not found</div>;
-  }
+    if (!singleProduct) {
+        return <div>Product not found</div>;
+    }
 
-  return (
-    <div className="mt-20">
-      <ProductDetailsClient
-        product={singleProduct}
-        relatedProduct={relatedProduct}
-      />
-    </div>
-  );
+    return (
+        <div className="mt-20">
+            <ProductDetailsClient product={singleProduct} relatedProduct={relatedProduct} />
+        </div>
+    );
 };
 
 export default ProductDetails;
