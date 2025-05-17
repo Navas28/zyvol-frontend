29fbc25426246948e67b864082407fa62707642b
Navas . M <navasmhd79@gmail.com>
2025-05-17 17:38:26 +0530

    update

--- app/(root)/product/product-details/[id]/page.tsx
+++ app/(root)/product/product-details/[id]/page.tsx
@@ -2,33 +2,38 @@ import { getProductByCategory, getSingleProduct } from "@/Request/requests";
 import ProductDetailsClient from "../../ProductDetailsClient";
 import { Product } from "@/typing";
 
+type Params = {
+  params: {
+    id: string;
+  };
+};
 
-const ProductDetails = async ({params,} : {params: {id: string}}) => {
-    const id = params.id;
-    let singleProduct: Product | null = null;
-    let relatedProduct: Product[] = [];
+const ProductDetails = async ({ params }: Params) => {
+  const id = params.id;
+  let singleProduct: Product | null = null;
+  let relatedProduct: Product[] = [];
 
-    try {
-        singleProduct = await getSingleProduct(id);
-        if (singleProduct) {
-            relatedProduct = await getProductByCategory(singleProduct.category);
-        }
-    } catch (error) {
-        console.error('Error fetching product data:', error);
+  try {
+    singleProduct = await getSingleProduct(id);
+    if (singleProduct) {
+      relatedProduct = await getProductByCategory(singleProduct.category);
     }
+  } catch (error) {
+    console.error("Error fetching product data:", error);
+  }
 
-    if (!singleProduct) {
-        return <div>Product not found</div>;
-    }
+  if (!singleProduct) {
+    return <div>Product not found</div>;
+  }
 
-    return (
-        <div className="mt-20">
-            <ProductDetailsClient 
-                product={singleProduct}
-                relatedProduct={relatedProduct}
-            />
-        </div>
-    );
+  return (
+    <div className="mt-20">
+      <ProductDetailsClient
+        product={singleProduct}
+        relatedProduct={relatedProduct}
+      />
+    </div>
+  );
 };
 
 export default ProductDetails;
